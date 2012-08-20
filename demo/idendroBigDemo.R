0342640c729ddc117ae9a9976ae0197d220e5241
tsieger <tomas@sieger>
2012-08-20 11:27:15 +0200

    local hack removed

--- demo/idendroBigDemo.R
+++ demo/idendroBigDemo.R
@@ -4,9 +4,6 @@
 # load "big data"
 data(hca5000)
 
-    # hack when run not from an installed package
-    if (!exists('hx')) load('~/src/R/idendro/idendro/data/hca5000.rda')
-
 colnames(x)<-1:ncol(x)
 rownames(x)<-1:nrow(x)
 
