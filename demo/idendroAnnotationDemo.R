16feab2b6287097bb76694a42181aecffb97df69
tsieger <tomas@sieger>
2012-08-20 11:35:16 +0200

    doc added; annotation size made proportional to annotation length

---
+++ demo/idendroAnnotationDemo.R
@@ -0,0 +1,17 @@
+## Simple demo showing how `idendro' visualizes a dendrogram.
+##
+
+# generate data in feature space
+n<-10
+x<-data.frame(very.long.dim.name1=c(rnorm(n,-1),rnorm(n,1)),long.dim.name2=c(rnorm(n,-1),rnorm(n,1)))
+rownames(x)<-paste('my brand new observation',1:(2*n))
+
+# compute pairwise distances
+dx<-dist(x)
+
+# perform hierarchical clustering
+hx<-hclust(dx)
+
+# visualize clusters
+idendro(hx,x)
+
