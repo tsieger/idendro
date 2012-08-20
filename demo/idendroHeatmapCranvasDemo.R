3bcf019b7d56dfc93f7de71604bf7f8d03b8fc95
tsieger <tomas@sieger>
2012-08-20 12:53:29 +0200

    cleaning

--- demo/idendroHeatmapCranvasDemo.R
+++ demo/idendroHeatmapCranvasDemo.R
@@ -5,11 +5,8 @@
 require(cranvas)
 
 n<-10
-#x<-cbind(rnorm(n),rnorm(n))
 x<-cbind(x1=1:10,x2=c(1,2,3,4,5,9,9,9,9,9))
-#x<-cbind(rnorm(n))
 rownames(x)<-1:n
-#colnames(x)<-paste('dim',1:2)
 cat('computing dist\n')
 dx<-dist(x)
 cat('computing HCA\n')
@@ -20,6 +17,4 @@ qx$.size<-jitter(qx$.size,.1)
 print(qscatter(x1,x2,data=qx,xlim=c(0,11),ylim=c(0,10)))
 
 cat('plotting HCA\n')
-idendro(hx,x,qx)
-
-cat('done\n')
+idendro(hx,qx)
