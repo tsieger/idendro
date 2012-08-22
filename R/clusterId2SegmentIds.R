313af464a7e6f1a1efb013cc0dc0011663f9e21a
tsieger <tomas@sieger>
2012-08-22 11:40:29 +0200

    cleaning code: addressing warnigns identified by R CMD check

--- R/clusterId2SegmentIds.R
+++ R/clusterId2SegmentIds.R
@@ -1,8 +1,8 @@
 clusterId2SegmentIds<-function
-## Convert cluster IDs to indices of dendrogram segments.
+### Convert cluster IDs to indices of dendrogram segments.
 (
     ids ##<< cluster IDs
 ) {
     return(rep(3*(ids-1),each=3)+(1:3))
-    #### segment IDs
+    ### segment IDs
 }
