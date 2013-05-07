a56c3a0e84018ca3812919298a8d31cd369f6461
Tomas Sieger <tsieger@server.fake>
2013-05-07 15:57:11 +0200

    tour demo added

---
+++ demo/idendroTourDemo.R
@@ -0,0 +1,20 @@
+## Demo showing `idendro' with tour feature-space projection of the data
+## (via the tourr package).
+##
+
+require(tourr)
+
+x<-flea[,1:6]
+# compute pairwise distances
+dx<-dist(x)
+
+# perform hierarchical clustering
+hx<-hclust(dx)
+
+# visualize clusters
+qx<-idendro(hx,x,maxClusterCount=6,doSmoothHeatmap=T)
+# now select some clusters in the dendrogram
+
+# once you are done, run this code to visualize data on a tour plot:
+#animate_xy(x,col=qx$.color)
+
