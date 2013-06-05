787ef4c3b55bdb7aa682a14bcdb24ed5b13068d0
Tomas Sieger <tsieger@server.fake>
2013-06-05 15:17:07 +0200

    update

--- demo/idendroAnnotationDemo.R
+++ demo/idendroAnnotationDemo.R
@@ -1,9 +1,11 @@
-## Simple demo showing how `idendro' visualizes a dendrogram.
+## Simple demo showing how `idendro' visualizes long dimnames and case
+## names in the heat map.
 ##
 
 # generate data in feature space
 n<-10
-x<-data.frame(very.long.dim.name1=c(rnorm(n,-1),rnorm(n,1)),long.dim.name2=c(rnorm(n,-1),rnorm(n,1)))
+x<-data.frame(very.long.dim.name1=c(rnorm(n,-1),rnorm(n,1)),
+    long.dim.name2=c(rnorm(n,-1),rnorm(n,1)))
 rownames(x)<-paste('my brand new observation',1:(2*n))
 
 # compute pairwise distances
@@ -14,4 +16,3 @@ hx<-hclust(dx)
 
 # visualize clusters
 idendro(hx,x)
-
