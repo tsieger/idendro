3dac7647e4908a37032813718d1851339961cfdb
Tomas Sieger <tsieger@server.fake>
2013-08-25 19:22:11 +0200

    test enabled, v1.3.1 created

---
+++ tests/testXType.R
@@ -0,0 +1,20 @@
+## Test different types of the data matrix `x' argument to idendro.
+##
+
+if (require(testthat)) {
+    test_that('different types of x argument work',{
+        require(idendro)
+
+        x<-1:10
+        idendro(hclust(dist(x)),x)
+
+        x<-as.character(1:10)
+        idendro(hclust(dist(x)),x)
+
+        x<-matrix(1:10,10)
+        idendro(hclust(dist(x)),x)
+
+        x<-data.frame(1:10)
+        idendro(hclust(dist(x)),x)
+    })
+}
