865478b7895567943dd88eaac77da671e6f3b0c9
Tomas Sieger <tsieger@server.fake>
2014-09-18 17:20:53 +0200

    PCA added

--- demo/idendroTour.R
+++ demo/idendroTour.R
@@ -1,5 +1,7 @@
-## Demo of dendrogram + heat map integrated with "tour" feature-space
-## projection of the data (via the `tourr' package).
+## Demo of dendrogram + heat map integrated with two more feature space
+## projections: a scatter plot depicting the first two principal
+## components of the data, and a "tour" projection (via the `tourr'
+## package).
 ##
 
 require(tourr) # animate_xy
@@ -13,8 +15,15 @@ hx<-hclust(dx)
 
 # visualize clusters
 qx<-idendro(hx,x,maxClusterCount=6,doSmoothHeatmap=T)
+
+# compute PCA ...
+x.pc <- prcomp(x,center=TRUE,scale.=TRUE)
+qx$PC1 <- x.pc$x[, 1]
+qx$PC2 <- x.pc$x[, 2]
+# ... and show the first two principal components
+qscatter(PC1, PC2, qx)
+
 # now select some clusters in the dendrogram
-
 # once you are done, run this code to visualize data on a tour plot:
-#animate_xy(x,col=qx$.color)
-
+#
+#  animate_xy(x,col=qx$.color)
