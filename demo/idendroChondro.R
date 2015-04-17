0d20fa169c2f1378b2d1c9c9a820cd2d2457804e
Tomas Sieger <tsieger@server.fake>
2015-04-17 19:03:38 +0200

    demo update

--- demo/idendroChondro.R
+++ demo/idendroChondro.R
@@ -4,17 +4,31 @@
 
 library(hyperSpec) # chondro data
 
-# extract data matrix
-x <- chondro [[]]
-# prepend 'x' in front of numeric dimnames
-colnames(x)<-paste('x',colnames(x),sep='')
+# preprocess chondro data
+cat('preprocessing the chondro data (it takes a while)\n')
+chondro <- spc.loess(chondro, newx = seq (602, 1800, by = 4))
+chondro <- chondro - spc.fit.poly.below(chondro)
+chondro <- sweep(chondro, 1, rowMeans(chondro), "/")
+overall.composition <- quantile(chondro, 0.05)
+chondro <- sweep(chondro, 2, overall.composition, "-")
+
 # HCA
-dx<-dist(x)
-hx<-hclust(dx)
+dst <- dist(chondro)
+dndr <- hclust(dst, method = "ward")
+
 # mutable data frame construction
-qx<-qdata(x)
-idendro(hx,qx,maxClusterCount=6)
+mdf.chondro <- as.wide.df(chondro)
+colnames(mdf.chondro)[-(1:3)] <- paste("wl", colnames(mdf.chondro)[-(1:3)], sep = ".")
+names <- as.character(wl(chondro))
+names[wl(chondro) %% 50 != 0] <- ""
+mdf.chondro <- qdata(mdf.chondro)
+
+# dendrogram
+idendro(dndr, mdf.chondro, heatmapRelSize = 0.75, heatmapColors = alois.palette(25))
+
 # scatter plot
-print(qscatter(x602,x606,data=qx,unibrushcolor=FALSE))
+print(qscatter(x, y, data = mdf.chondro, unibrushcolor = FALSE))
+
 # parallel coordinate plot
-print(qparallel(~.,data=qx))
+print(qparallel(vars = var_names(~., mdf.chondro)[-(1:3)], data = mdf.chondro,
+  names = names, scale = "I", glyph = "line"))
