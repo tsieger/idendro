f88594aaaca34d33cb01d1120e6f6460f1ee4cf8
Tomas Sieger <tsieger@server.fake>
2015-04-17 19:51:47 +0200

    pretty formatting, adding library(idendro)

--- demo/idendroAnnotation.R
+++ demo/idendroAnnotation.R
@@ -2,17 +2,19 @@
 ## names in the heat map.
 ##
 
+library(idendro) # idendro
+
 # generate data in feature space
-n<-10
-x<-data.frame(very.long.dim.name1=c(rnorm(n,-1),rnorm(n,1)),
-    long.dim.name2=c(rnorm(n,-1),rnorm(n,1)))
-rownames(x)<-paste('my brand new observation',1:(2*n))
+n <- 10
+x <- data.frame(very.long.dim.name1 = c(rnorm(n, -1), rnorm(n, 1)),
+    long.dim.name2 = c(rnorm(n, -1), rnorm(n, 1)))
+rownames(x) <- paste('my brand new observation', 1:(2*n))
 
 # compute pairwise distances
-dx<-dist(x)
+dx <- dist(x)
 
 # perform hierarchical clustering
-hx<-hclust(dx)
+hx <- hclust(dx)
 
 # visualize clusters
-idendro(hx,x)
+idendro(hx, x)
