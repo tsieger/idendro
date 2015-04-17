4898aca0d71d0b2d5e67fa58ee6e86228a9aaaae
Tomas Sieger <tsieger@server.fake>
2015-04-17 17:19:20 +0200

    pretty formatting

--- demo/idendroAgnesDiana.R
+++ demo/idendroAgnesDiana.R
@@ -2,19 +2,18 @@
 ## `cluster::diana'.
 ##
 
-require(cluster)
-
+library(cluster)
 
 data(iris)
 
-dx<-dist(iris[, 1:4])
+dx <- dist(iris[, 1:4])
 
 # perform hierarchical clustering using `agnes'
-hx.agnes<-agnes(dx)
+hx.agnes <- agnes(dx)
 # visualize clusters
-idendro(hx.agnes,iris)
+idendro(hx.agnes, iris)
 
 # perform hierarchical clustering using `diana'
-hx.diana<-diana(dx)
+hx.diana <- diana(dx)
 # visualize clusters
-idendro(hx.diana,iris)
+idendro(hx.diana, iris)
