b548bdbe955360e9b12e1fddda56e26976e51ba0
Tomas Sieger <tsieger@server.fake>
2015-04-17 17:14:30 +0200

    require -> library

--- demo/idendroWithCranvas.R
+++ demo/idendroWithCranvas.R
@@ -2,24 +2,27 @@
 ## integrated.
 ##
 
-require(cranvas)
+library(cranvas)
 
 data(iris)
 
 # compute pairwise distances
-dx<-dist(iris[, 1:4])
+dx <- dist(iris[, 1:4])
 
 # perform hierarchical clustering
-hx<-hclust(dx)
+hx <- hclust(dx)
 
 # prepare mutable data frame enabling bidirectional communication
 # between qscatter (coloring observations according to clusters
 # currently selected by idendro) and idendro (displaying brushed
 # observations)
-qx<-qdata(iris)
+qx <- qdata(iris)
 
 # visualize clusters
-idendro(hx,qx,maxClusterCount=6)
+idendro(hx, qx)
 
 # visualize data on a scatter plot
-print(qscatter(Sepal.Length,Sepal.Width,data=qx))
+print(qscatter(Sepal.Length, Sepal.Width, data = qx))
+# and a parallel coordinate plot
+print(qparallel(~., data = qx))
+
